551c31b7148de26e3362052b03dd540b39c3440b
RAGAdox <rishitech882@gmail.com>
2021-05-14 23:19:44 +0530

    Login

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,30 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    didRedirect: false,
+  });
+  const { email, password, error, loading, didRedirect } = values;
+  const handelChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+    console.log(email);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("Email =>" + email);
+    console.log("Password =>" + password);
+  };
   return (
     <div className="w-full h-screen text-gray-50 flex items-center justify-center content-center">
-      <div className="flex flex-col w-full lg:max-w-lg p-10 bg-green-900 bg-opacity-50 rounded-lg shadow-lg m-6 animate-title-fade-up">
+      <form className="flex flex-col w-full lg:max-w-lg p-10 bg-green-900 bg-opacity-50 rounded-lg shadow-lg m-6 animate-title-fade-up">
         <div className="flex flex-col mb-6">
-          <label className="justify-self-start mb-2">Username</label>
+          <label className="justify-self-start mb-2" htmlFor="username">
+            Username
+          </label>
           <input
             type="text"
             id="username"
+            value={email}
+            placeholder="Username"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={handelChange("email")}
           />
         </div>
         <div className="flex flex-col mb-6">
-          <label className="justify-self-start mb-2">Password</label>
+          <label className="justify-self-start mb-2" htmlFor="password">
+            Password
+          </label>
           <input
             type="password"
-            id="username"
+            id="password"
+            value={password}
+            onChange={handelChange("password")}
+            placeholder="Password"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
 
-        <button className="mt-2 bg-green-600 hover:bg-green-800 text-gray-100 font-semibold py-2 px-4 border border-green-400 rounded shadow">
+        <button
+          onClick={onSubmit}
+          className="mt-2 bg-green-600 hover:bg-green-800 text-gray-100 font-semibold py-2 px-4 border border-green-400 rounded shadow"
+        >
           Login
         </button>
-      </div>
+      </form>
     </div>
   );
 };
